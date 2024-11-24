b558dc3cfe4c8fc7517ac161cf9a755addb1cdfe
HebatullahHesham <114872807+HebatullahHesham@users.noreply.github.com>
2024-11-24 18:56:03 +0200

    Update webui.sh

--- webui.sh
+++ webui.sh
@@ -1,31 +1,26 @@
 #!/usr/bin/env bash
 
-
 # If run from macOS, load defaults from webui-macos-env.sh
 if [[ "$OSTYPE" == "darwin"* ]]; then
     export TORCH_COMMAND="pip install torch==1.12.1 torchvision==0.13.1"
 fi
 
 # python3 executable
-if [[ -z "${python_cmd}" ]]
-then
-    python_cmd="python3"
+if [[ -z "${python_cmd}" ]]; then
+    python_cmd="python3.10"
 fi
 
 # git executable
-if [[ -z "${GIT}" ]]
-then
+if [[ -z "${GIT}" ]]; then
     export GIT="git"
 fi
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-if [[ -z "${venv_dir}" ]]
-then
+if [[ -z "${venv_dir}" ]]; then
     venv_dir="venv"
 fi
 
-if [[ -z "${LAUNCH_SCRIPT}" ]]
-then
+if [[ -z "${LAUNCH_SCRIPT}" ]]; then
     LAUNCH_SCRIPT="launcher.py"
 fi
 
@@ -33,8 +28,7 @@ fi
 can_run_as_root=1
 
 # read any command line flags to the webui.sh script
-while getopts "f" flag > /dev/null 2>&1
-do
+while getopts "f" flag > /dev/null 2>&1; do
     case ${flag} in
         f) can_run_as_root=1;;
         *) break;;
@@ -56,8 +50,7 @@ printf "\e[1m\e[34mTested on Debian 11 (Bullseye)\e[0m"
 printf "\n%s\n" "${delimiter}"
 
 # Do not run as root
-if [[ $(id -u) -eq 0 && can_run_as_root -eq 0 ]]
-then
+if [[ $(id -u) -eq 0 && can_run_as_root -eq 0 ]]; then
     printf "\n%s\n" "${delimiter}"
     printf "\e[1m\e[31mERROR: This script must not be launched as root, aborting...\e[0m"
     printf "\n%s\n" "${delimiter}"
@@ -68,37 +61,40 @@ else
     printf "\n%s\n" "${delimiter}"
 fi
 
-if [[ -d .git ]]
-then
+# Ensure the script runs from the correct directory
+if [[ ! -d "${PWD}/SadTalker-WebUI" ]]; then
     printf "\n%s\n" "${delimiter}"
-    printf "Repo already cloned, using it as install directory"
+    printf "Switching to SadTalker-WebUI directory..."
     printf "\n%s\n" "${delimiter}"
-    install_dir="${PWD}/../"
-    clone_dir="${PWD##*/}"
+    cd /content/SadTalker-WebUI || exit 1
 fi
 
 # Check prerequisites
 gpu_info=$(lspci 2>/dev/null | grep VGA)
 case "$gpu_info" in
-    *"Navi 1"*|*"Navi 2"*) export HSA_OVERRIDE_GFX_VERSION=10.3.0
-    ;;
-    *"Renoir"*) export HSA_OVERRIDE_GFX_VERSION=9.0.0
+    *"Navi 1"*|*"Navi 2"*) export HSA_OVERRIDE_GFX_VERSION=10.3.0 ;;
+    *"Renoir"*) 
+        export HSA_OVERRIDE_GFX_VERSION=9.0.0
         printf "\n%s\n" "${delimiter}"
         printf "Experimental support for Renoir: make sure to have at least 4GB of VRAM and 10GB of RAM or enable cpu mode: --use-cpu all --no-half"
-        printf "\n%s\n" "${delimiter}"
-    ;;
-    *) 
-    ;;
+        printf "\n%s\n" "${delimiter}" ;;
+    *) ;;
 esac
-if echo "$gpu_info" | grep -q "AMD" && [[ -z "${TORCH_COMMAND}" ]]
-then
-    export TORCH_COMMAND="pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/rocm5.2"
-fi  
 
-for preq in "${GIT}" "${python_cmd}"
-do
-    if ! hash "${preq}" &>/dev/null
-    then
+if echo "$gpu_info" | grep -q "AMD" && [[ -z "${TORCH_COMMAND}" ]]; then
+    export TORCH_COMMAND="pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/rocm5.2"
+fi
+
+# Install python3.10-venv if not present (Debian-based systems)
+if ! dpkg -l | grep -q python3.10-venv; then
+    printf "\n%s\n" "${delimiter}"
+    printf "Installing python3.10-venv package..."
+    printf "\n%s\n" "${delimiter}"
+    sudo apt-get update && sudo apt-get install -y python3.10-venv
+fi
+
+for preq in "${GIT}" "${python_cmd}"; do
+    if ! hash "${preq}" &>/dev/null; then
         printf "\n%s\n" "${delimiter}"
         printf "\e[1m\e[31mERROR: %s is not installed, aborting...\e[0m" "${preq}"
         printf "\n%s\n" "${delimiter}"
@@ -106,8 +102,7 @@ do
     fi
 done
 
-if ! "${python_cmd}" -c "import venv" &>/dev/null
-then
+if ! "${python_cmd}" -c "import venv" &>/dev/null; then
     printf "\n%s\n" "${delimiter}"
     printf "\e[1m\e[31mERROR: python3-venv is not installed, aborting...\e[0m"
     printf "\n%s\n" "${delimiter}"
@@ -117,16 +112,15 @@ fi
 printf "\n%s\n" "${delimiter}"
 printf "Create and activate python venv"
 printf "\n%s\n" "${delimiter}"
-cd "${install_dir}"/"${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
-if [[ ! -d "${venv_dir}" ]]
-then
+
+if [[ ! -d "${venv_dir}" ]]; then
     "${python_cmd}" -m venv "${venv_dir}"
     first_launch=1
 fi
-# shellcheck source=/dev/null
-if [[ -f "${venv_dir}"/bin/activate ]]
-then
-    source "${venv_dir}"/bin/activate
+
+# Activate virtual environment
+if [[ -f "${venv_dir}/bin/activate" ]]; then
+    source "${venv_dir}/bin/activate"
 else
     printf "\n%s\n" "${delimiter}"
     printf "\e[1m\e[31mERROR: Cannot activate python venv, aborting...\e[0m"
